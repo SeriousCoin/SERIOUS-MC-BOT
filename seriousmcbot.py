ddd1462ae0c5997820e03941b579a55e4a3fac80
SeriousCoin <team@seriouscoin.xyz>
2024-05-19 17:53:55 -0400

    SERIOUS MC BOT V0.7
    - Using bot instead of client for bot commands

--- seriousmcbot.py
+++ seriousmcbot.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import requests
 import os
 from flask import Flask, jsonify
@@ -10,31 +11,27 @@ COIN_SYMBOL = 'SERIOUS'
 # Initialize the bot
 intents = discord.Intents.default()
 intents.messages = True
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command()
+async def marketcap(ctx):
+    # Fetch data from Dexscreener API
+    url = 'https://api.dexscreener.io/latest/dex/pairs/cronos/0x18ab7692cc20F68A550b1Fdd749720CAd4a4894F'
+    response = requests.get(url)
 
-    if message.content.startswith('!marketcap'):
-        # Fetch data from Dexscreener API
-        url = f'https://api.dexscreener.io/latest/dex/pairs/cronos/0x18ab7692cc20F68A550b1Fdd749720CAd4a4894F'
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            data = response.json()
-            if 'pair' in data:
-                market_cap = data['pair']['market_cap']
-                await message.channel.send(f'The market cap of {COIN_SYMBOL} is {market_cap}')
-            else:
-                await message.channel.send(f'Could not find market cap for {COIN_SYMBOL}')
+    if response.status_code == 200:
+        data = response.json()
+        if 'pair' in data:
+            market_cap = data['pair']['market_cap']
+            await ctx.send(f'The market cap of {COIN_SYMBOL} is {market_cap}')
         else:
-            await message.channel.send(f'Error fetching data: {response.status_code}')
+            await ctx.send(f'Could not find market cap for {COIN_SYMBOL}')
+    else:
+        await ctx.send(f'Error fetching data: {response.status_code}')
 
 # Flask app for health check
 app = Flask(__name__)
@@ -48,16 +45,14 @@ def health_check():
     return jsonify({'status': 'UP'})
 
 def run_discord_bot():
-    client.run(DISCORD_TOKEN)
+    bot.run(DISCORD_TOKEN)
 
 if __name__ == '__main__':
     import threading
-    from concurrent.futures import ThreadPoolExecutor
 
     # Start the Flask app in a separate thread
     flask_thread = threading.Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': int(os.environ.get('PORT', 5000))})
     flask_thread.start()
 
     # Start the Discord bot
-    with ThreadPoolExecutor() as executor:
-        executor.submit(run_discord_bot)
+    run_discord_bot()
